eaba8955ee0f34bbe4c71cbc2c1102b4edc00b60
JohnsonKeiriz <johnson1982@sbcglobal.net>
2016-11-21 13:54:59 -0600

    updated the faces to start from 0 instead of 1 normalize all the vertices and center them around the origin

--- js/polyhedron.js
+++ js/polyhedron.js
@@ -10,8 +10,22 @@
  */
 function Platonics() {
 
-    this.vertices = [];
-    this.faces = [];
+    var vertices = [];
+    var faces = [];
+    var numberOfVerticesPerFace = 0;
+
+    this.getVertices = function() { return vertices; };
+    this.getFaces = function() { return faces; };
+    this.getNumberOfFaces = function() { return faces.length; };
+    this.getNumberOfVerticesPerFace = function() { return  numberOfVerticesPerFace;};
+
+    this.getFace = function(i) {
+        var face = new Array(numberOfVerticesPerFace, new Array(3));
+        for (var j = 0; j < numberOfVerticesPerFace; j++) {
+            face[j] = vertices[faces[i][j]];
+        }
+        return face;
+    };
 
     this.createTetrahedron = function() {
 
@@ -23,6 +37,9 @@ function Platonics() {
                  [0, 2, 3],
                  [0, 3, 1],
                  [3, 2, 1]];
+
+        numberOfVerticesPerFace = 3;
+        centerAndNormalize();
     };
 
     this.createCube = function() {
@@ -36,12 +53,15 @@ function Platonics() {
                     [0,	1, 1],
                     [1,	1, 1]];
 
-        faces = [[1, 3, 4, 2],
-                 [5, 6, 8, 7],
-                 [2, 4, 8, 6],
-                 [1, 5, 7, 3],
-                 [1, 2, 6, 5],
-                 [3, 7, 8, 4]];
+        faces = [[0, 2, 3, 1],
+                 [4, 5, 7, 6],
+                 [1, 3, 7, 5],
+                 [0, 4, 6, 2],
+                 [0, 1, 5, 4],
+                 [2, 6, 7, 3]];
+
+        numberOfVerticesPerFace = 4;
+        centerAndNormalize();
     };
 
     this.createOctahedron = function() {
@@ -53,14 +73,17 @@ function Platonics() {
                     [0, 0, 1],
                     [0, 0, -1]];
 
-        faces = [[1, 2, 5],
-                 [2, 3, 5],
-                 [3, 4, 5],
-                 [4, 1, 5],
-                 [1, 6, 2],
-                 [2, 6, 3],
-                 [3, 6, 4],
-                 [1, 4, 6]];
+        faces = [[0, 1, 4],
+                 [1, 2, 4],
+                 [2, 3, 4],
+                 [3, 0, 4],
+                 [0, 5, 1],
+                 [1, 5, 2],
+                 [2, 5, 3],
+                 [0, 3, 5]];
+
+        numberOfVerticesPerFace = 3;
+        centerAndNormalize();
     };
 
     this.createDodecahedron = function() {
@@ -86,19 +109,21 @@ function Platonics() {
                     [ 1.0000, -0.3820,       0],
                     [ 1.0000,  0.3820,       0]];
 
-        faces =[[14, 18, 12,  6,  8],
-                [ 8,  4, 10, 16, 14],
-                [13, 15,  9,  3,  7],
-                [ 7,  5, 11, 17, 13],
-                [11, 12, 18, 20, 17],
-                [12, 11,  5,  2,  6],
-                [ 9, 10,  4,  1,  3],
-                [10,  9, 15, 19, 16],
-                [20, 18, 14, 16, 19],
-                [19, 15, 13, 17, 20],
-                [ 2,  5,  7,  3,  1],
-                [ 1,  4,  8,  6,  2]];
+        faces =[[13, 17, 11,  5,  7],
+                [ 7,  3,  9, 15, 13],
+                [12, 14,  8,  2,  6],
+                [ 6,  4, 10, 16, 12],
+                [10, 11, 17, 19, 16],
+                [11, 10,  4,  1,  5],
+                [ 8,  9,  3,  0,  2],
+                [ 9,  8, 14, 18, 15],
+                [19, 17, 13, 15, 18],
+                [18, 14, 12, 16, 19],
+                [ 1,  4,  6,  2,  0],
+                [ 0,  3,  7,  5,  1]];
 
+        numberOfVerticesPerFace = 5;
+        centerAndNormalize();
     };
 
     this.createIcosahedron = function() {
@@ -116,25 +141,36 @@ function Platonics() {
                     [ 0.6882, -0.5000, 1.3764],
                     [      0,       0, 1.9021]];
 
-        faces =[[ 1,  3,  2],
-                [ 1,  4,  3],
-                [ 1,  5,  4],
-                [ 1,  6,  5],
-                [ 1,  2,  6],
-                [ 2,  3,  7],
-                [ 3,  4,  8],
-                [ 4,  5,  9],
-                [ 5,  6, 10],
-                [ 6,  2, 11],
-                [ 7,  3,  8],
-                [ 8,  4,  9],
-                [ 9,  5, 10],
-                [10,  6, 11],
-                [11,  2,  7],
-                [ 7,  8, 12],
-                [ 8,  9, 12],
-                [ 9, 10, 12],
-                [10, 11, 12],
-                [11,  7, 12]];
+        faces =[[0, 2, 1],
+                [0, 3, 2],
+                [0, 4, 3],
+                [0, 5, 4],
+                [0, 1, 5],
+                [1, 2, 6],
+                [2, 3, 7],
+                [3, 4, 8],
+                [4, 5, 9],
+                [5, 1, 10],
+                [6, 2, 7],
+                [7, 3, 8],
+                [8, 4, 9],
+                [9, 5, 10],
+                [10, 1, 6],
+                [6, 7, 11],
+                [7, 8, 11],
+                [8, 9, 11],
+                [9, 10, 11],
+                [10, 6, 11]];
+
+        numberOfVerticesPerFace = 3;
+        centerAndNormalize();
     };
+
+    centerAndNormalize = function() {
+        var vCentroid = math.mean(vertices,0);
+        for (var i=0; i<vertices.length; i++) {
+            vertices[i] = math.subtract(vertices[i], vCentroid);
+            vertices[i] = math.divide(vertices[i], math.norm(vertices[i]));
+        }
+    }
 }
